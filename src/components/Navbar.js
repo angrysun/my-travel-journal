7c08bcc9de5c92317cded6911a620cfac8b25953
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-27 11:39:57 +0900

    navbar being built

---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <span><i class="fa-solid fa-earth-asia" id="globe-icon"></i> my japan travel journal</span>
+    </div>
+  )
+}
