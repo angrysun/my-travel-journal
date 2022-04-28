2f716c97ade6c5a009679c7bc7bad9ac8e23690f
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-28 11:57:33 +0900

    more cards added

--- src/data.js
+++ src/data.js
@@ -1,6 +1,15 @@
 const data = [
   {
     id: 1,
+    coverImg: "Nakameguro.jpg",
+    mapsLink: "https://goo.gl/maps/H31betmsgXGfMFAh9",
+    prefecture: "TOKYO",
+    placeLocation: "Meguro River",
+    date: "April 2021",
+    paragraphContent: "The Meguro River is 7.82 km (4.86 mi) in length and passes through Setagaya, Meguro and Shinagawa wards of Tokyo. The river banks are extensively landscaped and act as an urban green space for communities along its length. The river is a popular venue for cherry blossom viewing in the spring."
+  },
+  {
+    id: 2,
     coverImg: "Nara.jpg",
     mapsLink: "https://goo.gl/maps/NQE9Csds7ituyiDP8",
     prefecture: "NARA",
@@ -9,7 +18,7 @@ const data = [
     paragraphContent: "Nara Park is a public park located in the city of Nara, Japan, at the foot of Mount Wakakusa. Established in 1880 it is one of the oldest parks in Japan. According to local folklore, shika deer from this area were considered sacred due to a visit from Takemikazuchi, one of the four gods of Kasuga Shrine."
   },
   {
-    id: 2,
+    id: 3,
     coverImg: "Kinkakuji.jpg",
     mapsLink: "https://goo.gl/maps/D1N1Skb7UBnDbEZy9",
     prefecture: "KYOTO",
@@ -18,7 +27,7 @@ const data = [
     paragraphContent: "Kinkaku-ji is a Zen Buddhist temple and is one of the most popular buildings in Kyoto. It is designated as a National Special Historic Site, a National Special Landscape and is one of 17 locations making up the Historic Monuments of Ancient Kyoto which are World Heritage Sites."
   },
   {
-    id: 3,
+    id: 4,
     coverImg: "Dotombori.jpg",
     mapsLink: "https://goo.gl/maps/VA9e21cp8QVoEXLh8",
     prefecture: "OSAKA",
@@ -27,13 +36,31 @@ const data = [
     paragraphContent: "Dotombori is a popular nightlife and entertainment area characterized by its eccentric atmosphere and large illuminated signboards. One of the area's most prominent features, a billboard for confectionery company Glico displaying the image of a runner crossing a finishing line, is seen as an icon of Osaka within Japan."
   },
   {
-    id: 4,
+    id: 5,
     coverImg: "Zojoji.jpg",
     mapsLink: "https://goo.gl/maps/u4tzAFoZ5exq9PjY9",
     prefecture: "TOKYO",
     placeLocation: "Zojo-ji Temple",
     date: "December 2018",
     paragraphContent: "Zōjō-ji is a Jōdo-shū Buddhist temple in Tokyo. The temple's Sangadetsumon (main gate) is the oldest wooden building in Tokyo, dating from 1622. It is located in the Shiba neighborhood of Minato. Shiba Park is built around the temple, with Tokyo Tower standing beside it."
+  },
+  {
+    id: 6,
+    coverImg: "Skytree.jpg",
+    mapsLink: "https://g.page/TOKYOSKYTREE-official?share",
+    prefecture: "TOKYO",
+    placeLocation: "Tokyo Skytree",
+    date: "March 2019",
+    paragraphContent: "Tokyo Skytree is a broadcasting and observation tower in Sumida, Tokyo. It became the tallest structure in Japan in 2010 and reached its full height of 634 meters (2,080 ft) in March 2011, making it the tallest tower in the world. The tower is the primary television and radio broadcast site for the Kantō region."
+  },
+  {
+    id: 7,
+    coverImg: "Nakameguro.jpg",
+    mapsLink: "https://goo.gl/maps/H31betmsgXGfMFAh9",
+    prefecture: "TOKYO",
+    placeLocation: "Meguro River",
+    date: "April 2021",
+    paragraphContent: "The Meguro River is 7.82 km (4.86 mi) in length and passes through Setagaya, Meguro and Shinagawa wards of Tokyo. The river banks are extensively landscaped and act as an urban green space for communities along its length. The river is a popular venue for cherry blossom viewing in the spring."
   }
 ]
 
