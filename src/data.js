e09c5b452e19f42862dd25097a9a7371fe0f1cf8
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-28 11:22:11 +0900

    third card added

--- src/data.js
+++ src/data.js
@@ -9,13 +9,22 @@ const data = [
     paragraphContent: "Nara Park (奈良公園, Nara Kōen) is a public park located in the city of Nara, Japan, at the foot of Mount Wakakusa. Established in 1880 it is one of the oldest parks in Japan. According to local folklore, shika deer from this area were considered sacred due to a visit from Takemikazuchi, one of the four gods of Kasuga Shrine."
   },
   {
-    id: 1,
+    id: 2,
     coverImg: "Kinkakuji.jpg",
     mapsLink: "https://goo.gl/maps/D1N1Skb7UBnDbEZy9",
     prefecture: "KYOTO",
     placeLocation: "Kinkaku-ji",
     date: "August 2016",
     paragraphContent: "Kinkaku-ji is a Zen Buddhist temple and is one of the most popular buildings in Kyoto. It is designated as a National Special Historic Site, a National Special Landscape and is one of 17 locations making up the Historic Monuments of Ancient Kyoto which are World Heritage Sites."
+  },
+  {
+    id: 3,
+    coverImg: "Dotombori.jpg",
+    mapsLink: "https://goo.gl/maps/VA9e21cp8QVoEXLh8",
+    prefecture: "OSAKA",
+    placeLocation: "Dotombori",
+    date: "August 2016",
+    paragraphContent: "Dotombori is a popular nightlife and entertainment area characterized by its eccentric atmosphere and large illuminated signboards. One of the area's most prominent features, a billboard for confectionery company Glico displaying the image of a runner crossing a finishing line, is seen as an icon of Osaka within Japan."
   }
 ]
 
