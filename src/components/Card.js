2fd97a6c2cc65917c18e140418e646ed8c67cb40
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-28 10:17:09 +0900

    added second card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,18 @@
-import Nara from '../images/Nara.JPG'
-
-export default function Card() {
+export default function Card(props) {
   return (
     <div className='card--container'>
       <div className="card">
         <div className='card--image-container'>
-          {/* <img src={coverImg} alt="Nara" className="card--image" /> */}
+          <img src={`../images/${props.item.coverImg}`} alt="Nara" className="card--image" />
         </div>
         <div className='card--details'>
-          <a href="#" target="_blank" rel="noreferrer" id="location-icon"><i class="fa-solid fa-location-dot"></i></a>
-          <a href="#" target="_blank" rel="noreferrer" className='location--title'>NARA</a>
-          <a href="#" target="_blank" rel="noreferrer" className='location--maps'>View on Google Maps</a>
+          <a href={props.item.mapsLink} target="_blank" rel="noreferrer" id="location-icon"><i class="fa-solid fa-location-dot"></i></a>
+          <a href={props.item.mapsLink} target="_blank" rel="noreferrer" className='location--title'>{props.item.prefecture}</a>
+          <a href={props.item.mapsLink} target="_blank" rel="noreferrer" className='location--maps'>View on Google Maps</a>
           <div>
-            <h1 className='card--title'>Nara Park | Kofuku-ji Temple</h1>
-            <p className='card--date'>August 2018</p>
-            <p className='card--paragraph'>Nara Park (奈良公園, Nara Kōen) is a public park located in the city of Nara, Japan, at the foot of Mount Wakakusa. Established in 1880 it is one of the oldest parks in Japan. According to local folklore, shika deer from this area were considered sacred due to a visit from Takemikazuchi, one of the four gods of Kasuga Shrine.</p>
+            <h1 className='card--title'>{props.item.placeLocation}</h1>
+            <p className='card--date'>{props.item.date}</p>
+            <p className='card--paragraph'>{props.item.paragraphContent}</p>
           </div>
         </div>
       </div>
--- src/data.js
+++ src/data.js
@@ -2,11 +2,20 @@ const data = [
   {
     id: 1,
     coverImg: "Nara.jpg",
-    mapsLink: "",
-    prefLocation: "",
-    placeLocation: "",
-    date: "",
-    paragraphContent: ""
+    mapsLink: "https://goo.gl/maps/NQE9Csds7ituyiDP8",
+    prefecture: "NARA",
+    placeLocation: "Nara Park | Kofuku-ji Temple",
+    date: "July 2016",
+    paragraphContent: "Nara Park (奈良公園, Nara Kōen) is a public park located in the city of Nara, Japan, at the foot of Mount Wakakusa. Established in 1880 it is one of the oldest parks in Japan. According to local folklore, shika deer from this area were considered sacred due to a visit from Takemikazuchi, one of the four gods of Kasuga Shrine."
+  },
+  {
+    id: 1,
+    coverImg: "Kinkakuji.jpg",
+    mapsLink: "https://goo.gl/maps/D1N1Skb7UBnDbEZy9",
+    prefecture: "KYOTO",
+    placeLocation: "Kinkaku-ji",
+    date: "August 2016",
+    paragraphContent: "Kinkaku-ji is a Zen Buddhist temple and is one of the most popular buildings in Kyoto. It is designated as a National Special Historic Site, a National Special Landscape and is one of 17 locations making up the Historic Monuments of Ancient Kyoto which are World Heritage Sites."
   }
 ]
 
