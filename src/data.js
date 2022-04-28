4c71a323105ec879a2db73f6ce41188fe4bb4be5
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-28 11:34:07 +0900

    fourth card added

--- src/data.js
+++ src/data.js
@@ -4,16 +4,16 @@ const data = [
     coverImg: "Nara.jpg",
     mapsLink: "https://goo.gl/maps/NQE9Csds7ituyiDP8",
     prefecture: "NARA",
-    placeLocation: "Nara Park | Kofuku-ji Temple",
+    placeLocation: "Nara Park",
     date: "July 2016",
-    paragraphContent: "Nara Park (奈良公園, Nara Kōen) is a public park located in the city of Nara, Japan, at the foot of Mount Wakakusa. Established in 1880 it is one of the oldest parks in Japan. According to local folklore, shika deer from this area were considered sacred due to a visit from Takemikazuchi, one of the four gods of Kasuga Shrine."
+    paragraphContent: "Nara Park is a public park located in the city of Nara, Japan, at the foot of Mount Wakakusa. Established in 1880 it is one of the oldest parks in Japan. According to local folklore, shika deer from this area were considered sacred due to a visit from Takemikazuchi, one of the four gods of Kasuga Shrine."
   },
   {
     id: 2,
     coverImg: "Kinkakuji.jpg",
     mapsLink: "https://goo.gl/maps/D1N1Skb7UBnDbEZy9",
     prefecture: "KYOTO",
-    placeLocation: "Kinkaku-ji",
+    placeLocation: "Kinkaku-ji Temple",
     date: "August 2016",
     paragraphContent: "Kinkaku-ji is a Zen Buddhist temple and is one of the most popular buildings in Kyoto. It is designated as a National Special Historic Site, a National Special Landscape and is one of 17 locations making up the Historic Monuments of Ancient Kyoto which are World Heritage Sites."
   },
@@ -25,6 +25,15 @@ const data = [
     placeLocation: "Dotombori",
     date: "August 2016",
     paragraphContent: "Dotombori is a popular nightlife and entertainment area characterized by its eccentric atmosphere and large illuminated signboards. One of the area's most prominent features, a billboard for confectionery company Glico displaying the image of a runner crossing a finishing line, is seen as an icon of Osaka within Japan."
+  },
+  {
+    id: 4,
+    coverImg: "Zojoji.jpg",
+    mapsLink: "https://goo.gl/maps/u4tzAFoZ5exq9PjY9",
+    prefecture: "TOKYO",
+    placeLocation: "Zojo-ji Temple",
+    date: "December 2018",
+    paragraphContent: "Zōjō-ji is a Jōdo-shū Buddhist temple in Tokyo. The temple's Sangadetsumon (main gate) is the oldest wooden building in Tokyo, dating from 1622. It is located in the Shiba neighborhood of Minato. Shiba Park is built around the temple, with Tokyo Tower standing beside it."
   }
 ]
 
