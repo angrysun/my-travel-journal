2c02dcabf9ea6f6322d04ff53d385b376f5bacce
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-28 12:14:31 +0900

    making variables for dark mode

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
 import data from './data';
+import ToggleButton from './components/ToggleButton';
+
+let Checked = () => <>🌙</>;
+let Unchecked = () => <>🔆</>;
 
 function App() {
+  const storedDarkMode = JSON.parse(localStorage.getItem("dbc-dark-mode"));
+  const [darkMode, setDarkMode] = useState(storedDarkMode);
+  const toggleDarkMode = () => setDarkMode(!darkMode);
   const cards = data.map(item => {
     return <Card
       key={item.id}
@@ -11,9 +18,33 @@ function App() {
     />
   })
 
+  useEffect(() => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    if (prefersDark) {
+      setDarkMode(true);
+      Checked = () => <>🔆</>;
+      Unchecked = () => <>🌙</>;
+    } else {
+      setDarkMode(false);
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   console.log(`Is in dark mode? ${darkMode}`);
+  //   localStorage.setItem("dbc-dark-mode", darkMode);
+  // }, [darkMode]);
+
   return (
-    <div className="App">
+    <div className="App" data-theme={darkMode ? "dark" : "light"}>
       <Navbar />
+      <ToggleButton
+        onChange={toggleDarkMode}
+        icons={{ checked: <Checked />, unchecked: <Unchecked /> }}
+        aria-label="Dark mode"
+      />
       <section>
         {cards}
       </section>
